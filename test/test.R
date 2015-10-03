d1602fc515fbbfe7a2358a0f13ee7f4f847a4448
Zuguang Gu <z.gu@dkfz.de>
2015-10-03 10:46:35 +0000

    resolve conflict
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EnrichedHeatmap@109145 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ test/test.R
@@ -0,0 +1,24 @@
+
+library(ComplexHeatmap)
+library(GenomicFeatures)
+library(GenomicRanges)
+
+tss = promoters(genes, upstream = 0, downstream = 1)
+
+x1 = normalizeToMatrix(H3K4me3, tss, value_column = "coverage", extend = 5000, mean_mode = "w0", w = 100, smooth = TRUE)
+x2 = normalizeToMatrix(meth, tss, value_column = "meth", extend = 5000, w = 100, empty_value = 0.5, smooth = TRUE)
+
+EnrichedHeatmap(x1, name = "H3K4me3", column_title = "histone modification") + 
+EnrichedHeatmap(x2, name = "methylation", column_title = "methylation")
+
+EnrichedHeatmap(x1, name = "H3K4me3", column_title = "histone modification", 
+	split = sample(letters[1:2], length(tss), replace = TRUE))
+
+x = normalizeToMatrix(meth, cgi, value_column = "meth", extend = 5000, w = 100, empty_value = 0.5)
+
+EnrichedHeatmap(x2, name = "methylation", column_title = "methylation", axis_name_rot = -90)
+
+
+kmeans(mat1)$clusters
+EnrichedHeatmap(mat1, col = c("white", "red"), name = "H3K4me3", 
+	split = kmeans(mat1, centers =2)$cluster)
